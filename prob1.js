a83c0b4e87297fa6142845ef8e6ebc3e523218d8
DanikaMarieSPanares <danikamarie.panares@smc.pshs.edu.ph>
2025-01-27 10:20:52 +0800

    Update prob1.js
    started draft

--- prob1.js
+++ prob1.js
@@ -1,1 +1,14 @@
+<html>
+<body>
+ <script>
+ 
+ var nickname = prompt("Name:");
+ var height = prompt("Height:");
+ var weight = prompt("Weight:");	
 
+ alert("Name: " + nickname +  "</br> Height: " + height + "</br>" + "Weight: " + weight);
+
+
+ </script>
+</body>
+</html>
